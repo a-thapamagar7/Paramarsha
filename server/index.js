a445d2b240dbae56b7a5f9a0813fd49ffb8be860
unknown <ayushtmb7@mgail.com>
2024-08-25 01:37:09 +0545

    Fixed cors issue

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const loginRegister = require("./routes/User");
 const createQuestion = require("./routes/Mocktest");
